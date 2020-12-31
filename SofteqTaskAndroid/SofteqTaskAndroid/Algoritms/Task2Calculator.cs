5c53cf2a13e530e79fb8c9f9ec03fa814cdb33e2
Lise <glushchenko.L.A@yandex.by>
2020-12-31 17:05:26 +0300

    add task 2

---
+++ SofteqTaskAndroid/SofteqTaskAndroid/Algoritms/Task2Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+/*
+* Created by LiseGit at 31.12.2020.
+* 
+* */
+namespace SofteqTaskAndroid.Algoritms
+{
+    class Task2Calculator
+    {
+        private int w, t;
+        private List<int> runOut = new List<int>();
+
+        bool CheckInput(string input)
+        {
+            string pattern = @"^(\s*(4|6|8|10)\s+([4-9]|1[0-9]|20)\s*\n+)((\s*(([1-9](\d){0,2})|([1-2](\d){3})|3000)\s*\n+))+(\s*(([1-9](\d){0,2})|([1-2](\d){3})|3000)\s*\n*)\s*";
+            if (Regex.IsMatch(input, pattern))
+            {
+                string[] words = input.Split(new char[] { ' ', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+                w = Convert.ToInt16(words[0]);
+                t = Convert.ToInt16(words[1]);
+                if (t < w)
+                {
+                    return false;
+                }
+                else
+                {
+                    if(words.Length-2 != w)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        for(int i=2; i<words.Length; i++)
+                        {
+                            runOut.Add(Convert.ToInt16(words[i]));
+                        }
+                        return true;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        double calculateResult()
+        {
+            double result=0;
+            foreach (int run in runOut)
+            {
+                result += (double) 1 / run;
+            }
+            return Math.Round(t / result, 3);
+        }
+        public string getCalculatedResult(string input)
+        {
+            if (CheckInput(input) == false)
+            {
+                return "Incorrect input.";
+            }
+            else
+            {
+                return calculateResult().ToString();
+            }
+        }
+    }
+}
--- SofteqTaskAndroid/SofteqTaskAndroid/PageModels/Task2PageModel.cs
+++ SofteqTaskAndroid/SofteqTaskAndroid/PageModels/Task2PageModel.cs
@@ -1,14 +1,42 @@
 ﻿using FreshMvvm;
+using SofteqTaskAndroid.Algoritms;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 /*
- * Created by LiseGit at 28.12.2020.
- * 
- * */
+* Created by LiseGit at 28.12.2020.
+* 
+* */
 namespace SofteqTaskAndroid.PageModels
 {
-    class Task2PageModel : FreshBasePageModel
+    class Task2PageModel : FreshBasePageModel, INotifyPropertyChanged
     {
+        private string _result;
+        public string Input { get; set; }
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand CalculateCommand => new Command(Calculate);
+        private void Calculate()
+        {
+            Task2Calculator calculator = new Task2Calculator();
+            Result = calculator.getCalculatedResult(Input);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
