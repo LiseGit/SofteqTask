d4651041e2785b4dac6354d1954cac123b9e8cd5
Lise <glushchenko.L.A@yandex.by>
2020-12-31 18:04:02 +0300

    change pages view

--- SofteqTaskAndroid/SofteqTaskAndroid/Algoritms/Task1Calculator.cs
+++ SofteqTaskAndroid/SofteqTaskAndroid/Algoritms/Task1Calculator.cs
@@ -13,7 +13,7 @@ namespace SofteqTaskAndroid.Algoritms
     {
         private bool CheckInput(string input)
         {
-            string pattern = @"^\s*((((-?\d+)(.\d{1,3})))|([1-9]\d*)|0)\s*$";
+            string pattern = @"^\s*((((-?\d+)(.\d+)))|(-?[1-9]\d*)|0)\s*$";
             if (Regex.IsMatch(input, pattern))
             {
                 return true;
